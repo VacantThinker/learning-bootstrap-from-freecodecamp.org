ebc238beff383eff8830f5a6f28c72d90952bced
VacantThinker <50100840+VacantThinker@users.noreply.github.com>
2021-09-16 07:56:52 +0800

    a

---
+++ js/second.js
@@ -0,0 +1,17 @@
+// document.addEventListener("DOMContentLoaded", function () {
+
+    // document.querySelectorAll('button').forEach(ele => {
+    //     console.log(ele.classList);
+    //     ele.addEventListener("click", ev => {
+    //         console.log("ev")
+    //     })
+    //     ele.classList.add("animate__animated", "animate__bounce")
+    // })
+
+// })
+
+$(document).ready(function () {
+    $("button").addClass("animate__animated animate__bounce");
+    $(".well").addClass("animate__animated animate__shakeX");
+    $("#target3").addClass();
+});
