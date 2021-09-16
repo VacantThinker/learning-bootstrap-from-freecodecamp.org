2500ef4ab18adca8f9a696147422c38062e9b5c3
VacantThinker <50100840+VacantThinker@users.noreply.github.com>
2021-09-16 10:06:39 +0800

    'a'

--- js/second.js
+++ js/second.js
@@ -1,13 +1,10 @@
-// document.querySelectorAll('button').forEach(ele => {
-//     ele.classList.add("animate__animated", "animate__bounce")
-// })
+document.querySelectorAll('button').forEach(ele => {
+    ele.classList.add("animate__animated", "animate__bounce")
+})
 
-import '../jquery-3.6.0.slim.min.js'
 
-$(document).ready(function () {
-
-    $("button").addClass("animate__animated animate__bounce");
-    $(".well").addClass("animate__animated animate__shakeX");
-    $("#target3").addClass();
-
-});
+// $(document).ready(function () {
+//     $("button").addClass("animate__animated animate__bounce");
+//     $(".well").addClass("animate__animated animate__shakeX");
+//     $("#target3").addClass("animate__animated animate__fadeOut");
+// });
